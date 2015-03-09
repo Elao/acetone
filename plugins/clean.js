16971012612dbdb04dd7315cbfe24dd84d6bb7eb
nervo <nervo@nervo.net>
2015-03-09 22:58:32 +0100

    Use chalk for coloring command outputs (force to 0.5.*, due to no-color default from 1.0.*) Fix #1

--- plugins/clean.js
+++ plugins/clean.js
@@ -8,10 +8,11 @@ module.exports = function(acetone)
         gulpTask: function(callback) {
             var
                 gulpUtil = require('gulp-util'),
+                chalk    = require('chalk'),
                 path = acetone.fileSystem.getDestPath();
 
             if (!acetone.options.isSilent()) {
-                gulpUtil.log('Delete', gulpUtil.colors.magenta(path));
+                gulpUtil.log('Delete', chalk.magenta(path));
             }
 
             acetone.fileSystem.rimrafPath(path, callback);
--- plugins/list.js
+++ plugins/list.js
@@ -8,6 +8,7 @@ module.exports = function(acetone)
         gulpTask: function(callback) {
             var
                 gulpUtil = require('gulp-util'),
+                chalk    = require('chalk'),
                 bundles, libraries;
 
             // Parameters
@@ -21,12 +22,12 @@ module.exports = function(acetone)
             if (gulpUtil.env.bundles) {
                 bundles = acetone.bundles.find();
 
-                gulpUtil.log('Found', gulpUtil.colors.cyan(bundles.length), 'bundles');
+                gulpUtil.log('Found', chalk.cyan(bundles.length), 'bundles');
 
                 bundles.forEach(function(bundle) {
-                    gulpUtil.log('-', bundle.getId(), gulpUtil.colors.magenta(bundle.getPath()));
+                    gulpUtil.log('-', bundle.getId(), chalk.magenta(bundle.getPath()));
                     if (bundle.hasDescription()) {
-                        gulpUtil.log(' ', gulpUtil.colors.cyan(bundle.getDescription()));
+                        gulpUtil.log(' ', chalk.cyan(bundle.getDescription()));
                     }
                 });
             }
@@ -35,19 +36,19 @@ module.exports = function(acetone)
             if (gulpUtil.env.libraries) {
                 libraries = acetone.libraries.find();
 
-                gulpUtil.log('Found', gulpUtil.colors.cyan(libraries.length), 'libraries');
+                gulpUtil.log('Found', chalk.cyan(libraries.length), 'libraries');
 
                 libraries.forEach(function(library) {
-                    gulpUtil.log('-', gulpUtil.colors.magenta(library.getPath()));
+                    gulpUtil.log('-', chalk.magenta(library.getPath()));
                     if (library.hasDescription()) {
-                        gulpUtil.log(' ', gulpUtil.colors.cyan(library.getDescription()));
+                        gulpUtil.log(' ', chalk.cyan(library.getDescription()));
                     }
                 });
             }
 
             // List pools
             if (gulpUtil.env.pools) {
-                gulpUtil.log('Added', gulpUtil.colors.cyan(acetone.poolHandlers.length), 'pool handlers');
+                gulpUtil.log('Added', chalk.cyan(acetone.poolHandlers.length), 'pool handlers');
 
                 // Pool handlers
                 acetone.poolHandlers.forEach(function(poolHandler)
@@ -57,18 +58,18 @@ module.exports = function(acetone)
 
                     gulpUtil.log('-', poolHandler.getId());
                     if (poolHandler.hasDescription()) {
-                        gulpUtil.log(' ', gulpUtil.colors.cyan(poolHandler.getDescription()));
+                        gulpUtil.log(' ', chalk.cyan(poolHandler.getDescription()));
                     }
 
                     // Pools
                     pools = poolHandler.pools.find();
-                    gulpUtil.log('     Found', gulpUtil.colors.cyan(pools.length), 'pools');
+                    gulpUtil.log('     Found', chalk.cyan(pools.length), 'pools');
                     pools.forEach(function(pool) {
                         gulpUtil.log('     -', pool.getName());
-                        gulpUtil.log('      ', gulpUtil.colors.cyan('src: '), gulpUtil.colors.magenta(
+                        gulpUtil.log('      ', chalk.cyan('src: '), chalk.magenta(
                             pool.getSrc()
                         ));
-                        gulpUtil.log('      ', gulpUtil.colors.cyan('dest:'), gulpUtil.colors.magenta(
+                        gulpUtil.log('      ', chalk.cyan('dest:'), chalk.magenta(
                             poolHandler.getDestPath(pool.getDest())
                         ));
                     });
